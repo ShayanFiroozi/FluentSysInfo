09f24e97d2bb1a0ccb02cd36d30f924e34e4fef2
Shayan Firoozi <shayan.firoozi@gmail.com>
2024-03-27 00:27:12 +0330

    Adding the FluentSysInfo.Core project

---
+++ FluentSysInfo.Core/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Fluent System Information Core Module")]
+[assembly: AssemblyDescription("Provide the accessibility to the various System Information.")]
+[assembly: AssemblyProduct("FluentSysInfo.Core")]
+[assembly: AssemblyCopyright("Copyright © 2024 Shayan Firoozi (Under MIT License)")]
+[assembly: AssemblyTrademark("Under MIT License")]
+[assembly: AssemblyCompany("https://github.com/ShayanFiroozi")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.*")]
